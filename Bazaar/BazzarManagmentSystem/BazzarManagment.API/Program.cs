b3b128baabcb22eb522f1a5a5451ed790e76b4e6
AsmaaHamed <asmaa1545@gmail.com>
2019-09-02 20:40:28 +0200

    Committing initialProject Architecture:  1- DAL : contians  Project DB Models  --  initial Empty  2- DTO : Contians Models Used to transfer Data from UI to API and  vice Verse .  3- API : Contians Controllers and Action to be called in UI Using Ajax  Requests  4- BLL : Contians Services Logic to be Called in API Layer .  5- UI.MVC : contians MVC core Project for creating APPlication  Views and User Interaction Logic  6- Common Layer Contians :   a) Repo : repository Layer connect DAL Model with BLL services .    b) Logger : Contians Logging Logic Used to Log : Error , warrning ,     Exception , Info .    c) Contractors : Contains Interfaces to be Used In BLL Services .

--- Bazaar/BazzarManagmentSystem/BazzarManagment.API/Program.cs
+++ Bazaar/BazzarManagmentSystem/BazzarManagment.API/Program.cs
@@ -17,6 +17,7 @@ namespace BazzarManagment.API
             CreateWebHostBuilder(args).Build().Run();
         }
 
+
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();

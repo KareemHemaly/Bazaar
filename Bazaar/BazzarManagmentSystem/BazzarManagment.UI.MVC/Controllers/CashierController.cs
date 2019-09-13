355b83bf71bcdd8da9237f5da373a8abb00b5fa0
KareemHemaly <kareem.hemaly8@gmail.com>
2019-09-13 19:14:04 +0200

    Add OrderPage and Add Product Page

---
+++ Bazaar/BazzarManagmentSystem/BazzarManagment.UI.MVC/Controllers/CashierController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+//using System.Web.Mvc;
+
+namespace BazzarManagment.UI.MVC.Controllers
+{
+    public class CashierController : Controller
+    {
+        public IActionResult Order()
+        {
+            return View();
+        }
+
+        //protected void HandleUnknownAction(string actionName)
+        //{
+        //    this.View(actionName).ExecuteResult(this.ControllerContext);
+        //}
+    }
+}
